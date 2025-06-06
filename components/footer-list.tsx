0a3c4b0015909c3b44821aa793f80a415f0e2d9f
Acbox <acbox0328@gmail.com>
2025-06-06 16:16:18 +0800

    fix

--- components/footer-list.tsx
+++ components/footer-list.tsx
@@ -4,7 +4,7 @@ function FooterList({ title, items }: { title: string; items: [string, string][]
       <h2 className="text-lg text-black">{title}</h2>
       <div className="flex flex-col gap-1 text-[#454545]">
         {items.map((item) => (
-          <a href={item[1]}><span key={item[0]}>{item[0]}</span></a>
+          <a href={item[1]} key={item[0]}><span>{item[0]}</span></a>
         ))}
       </div>
     </div>
